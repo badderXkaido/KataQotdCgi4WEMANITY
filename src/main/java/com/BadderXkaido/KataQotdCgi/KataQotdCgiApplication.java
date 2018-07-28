3e4dc0bd22b462eb7b943dadaf942a6d269a299b
badderXkaido <badre.mouhoub@gmail.com>
2018-07-28 20:48:11 +0100

    synchronization data

---
+++ src/main/java/com/BadderXkaido/KataQotdCgi/KataQotdCgiApplication.java
@@ -0,0 +1,12 @@
+package com.BadderXkaido.KataQotdCgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataQotdCgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataQotdCgiApplication.class, args);
+	}
+}
